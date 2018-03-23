0ffd3f21eb221c6b23890952c897a3f35daae46e
darshanparab <mr.darshanparab@gmail.com>
2018-03-23 20:11:06 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from twitter import Twitter, OAuth
+from copy import deepcopy
 import json
 import requests
 import re
-import sys
+import csv 
 
 def hybridanalysis(tweets):
 	ha=[]
@@ -62,4 +63,64 @@ for user in tweetsrc:
 		intel+=search(feed)
 	del feeds
 tweetsrc.close()
-print(intel)
+
+intelex0=[]
+keys=['filename','md5','sha1','sha256','family','domain','host','vtstatus']
+for threat in intel:
+	temp=dict.fromkeys(keys)
+	if(len(threat['domains'])!=0):
+		for domain in threat['domains']:
+			temp['filename']=threat['submit_name']
+			temp['md5']=threat['md5']
+			temp['sha1']=threat['sha1']
+			temp['sha256']=threat['sha256']
+			temp['domain']=domain
+			temp['host']=threat['hosts']
+			temp['vtstatus']=threat['av_detect']
+			temp['family']=threat['vx_family']
+			intelex0.append(deepcopy(temp))
+	else:
+		temp['filename']=threat['submit_name']
+		temp['md5']=threat['md5']
+		temp['sha1']=threat['sha1']
+		temp['sha256']=threat['sha256']
+		temp['domain']=''
+		temp['host']=threat['hosts']
+		temp['vtstatus']=threat['av_detect']
+		temp['family']=threat['vx_family']
+		intelex0.append(deepcopy(temp))	
+	del temp
+
+intelex1=[]
+for threat in intelex0:
+	temp=dict.fromkeys(keys)
+	if(len(threat['host'])!=0):
+		for host in threat['host']:
+			temp['filename']=threat['filename']
+			temp['md5']=threat['md5']
+			temp['sha1']=threat['sha1']
+			temp['sha256']=threat['sha256']
+			temp['domain']=threat['domain']
+			temp['host']=host
+			temp['vtstatus']=threat['vtstatus']
+			temp['family']=threat['family']
+			intelex1.append(deepcopy(temp))
+	else:
+		temp['filename']=threat['filename']
+		temp['md5']=threat['md5']
+		temp['sha1']=threat['sha1']
+		temp['sha256']=threat['sha256']
+		temp['domain']=threat['domain']
+		temp['host']=''
+		temp['vtstatus']=threat['vtstatus']
+		temp['family']=threat['family']
+		intelex1.append(deepcopy(temp))	
+	del temp
+
+#intel=json.loads(intelex1)
+of=open('threatintel.csv','w')
+writer=csv.writer(of,delimiter=',',quotechar='"',quoting=csv.QUOTE_ALL)
+for threat in intelex1:
+	writer.writerow([threat['filename'],threat['md5'],threat['sha1'],threat['sha256'],threat['family'],threat['vtstatus'],threat['domain'],threat['host']])
+#	writer.writerow(threat)
+of.close()
